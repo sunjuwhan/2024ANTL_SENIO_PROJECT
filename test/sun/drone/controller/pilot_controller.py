05027de3212cbe24b2b6815afe6fd152aeaa0bf6
sunjuwhan <mathsun0011@gmail.com>
2024-04-01 16:30:38 +0900

    tses

--- test/sun/drone/controller/pilot_controller.py
+++ test/sun/drone/controller/pilot_controller.py
@@ -16,7 +16,6 @@ class PilotController:
         while True:
             (key,mode)=self.__pilot_model.get_data()
             (yaw,throttle,roll,pitch)=key.get_key()
-            print(yaw,throttle)
             if(mode==0):
                 await self.__drone.get_drone().manual_control.set_manual_control_input(pitch,yaw,throttle,roll)
               
