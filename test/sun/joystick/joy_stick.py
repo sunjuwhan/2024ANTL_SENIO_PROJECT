d987d3e5380727cab96045e2d07f4aad225f49fa
sunjuwhan <mathsun0011@gmail.com>
2024-04-01 15:59:08 +0900

    Update joy_stick.py

--- test/sun/joystick/joy_stick.py
+++ test/sun/joystick/joy_stick.py
@@ -18,7 +18,7 @@ def ReadChannel(channel,spi):
   data = ((adc[1]&3) << 8) + adc[2]
   return data
 
-def stabil_vrx(vrx):
+def stabil_vrx(vrx_pos):
   if(vrx_pos>=500 and vrx_pos<=510):
     vrx_pos=500
   elif(vrx_pos>=0 and vrx_pos < 3):
@@ -27,7 +27,7 @@ def stabil_vrx(vrx):
     vrx_pos=1000
   return vrx_pos
 
-def stabil_vry(vry):
+def stabil_vry(vry_pos):
   if(vry_pos>=495 and vry_pos<=510):
     vry_pos=500
   elif(vry_pos>=0 and vry_pos < 3):
@@ -36,7 +36,7 @@ def stabil_vry(vry):
     vry_pos=1000
   return vry_pos
     
-def stabil_vrx_2(vrx_2):
+def stabil_vrx_2(vrx_pos_2):
   if(vrx_pos_2>=518 and vrx_pos_2 <=528):
     vrx_pos_2=500
   elif (vrx_pos_2>=0 and vrx_pos_2 < 3):
@@ -45,7 +45,7 @@ def stabil_vrx_2(vrx_2):
     vrx_pos_2=1000
   return vrx_pos_2
 
-def stabil_vry_2(vry_2):
+def stabil_vry_2(vry_pos_2):
   if(vry_pos_2>=501 and vry_pos_2<=511):
     vry_pos_2=500
   elif (vry_pos_2>=0 and vry_pos_2<=5):
@@ -85,7 +85,7 @@ def run():
     
     mode=0
     msg=f"{vrx_pos} {vry_pos} {vrx_pos_2} {vry_pos_2} "+mode  #yaw throtle  roll pirch
-
+    print(msg)
     sock.sendto(msg.encode(),(HOST,PORT))
     time.sleep(delay)
   
