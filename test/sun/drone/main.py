6949ca43456d41ad77fa8e990f7fd9a1100ee823
sunjuwhan <mathsun0011@gmail.com>
2024-04-08 16:27:55 +0900

    Update main.py

--- test/sun/drone/main.py
+++ test/sun/drone/main.py
@@ -12,7 +12,7 @@ class Main():
         self.__controller=controller.MasterController(self.__pilot_model,self.__camera_model,self.__gps_model)
         self.__view= view.SocketView(self.__pilot_model,self.__camera_model)
     def run(self):
-        camera_thread=Thread(target=self.__controller.get_camera)
+        camera_thread=Thread(target=self.__controller.run_camera)
         camera_thread.run()
         self.__view.run()
         
