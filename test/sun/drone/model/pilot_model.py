a6e8937ee896556ec24de5e33a7cf7d779ed5fe0
sunjuwhan <mathsun0011@gmail.com>
2024-04-10 21:29:59 +0900

    Update pilot_model.py

--- test/sun/drone/model/pilot_model.py
+++ test/sun/drone/model/pilot_model.py
@@ -23,6 +23,9 @@ class Drone:
             if health.is_global_position_ok and health.is_home_position_ok:
                 print("-- Global position state is good enough for flying.")
                 break
+        await self.antl_drone.manual_control.set_manual_control_input(
+        float(0), float(0), float(0.5), float(0)
+    )
         print("-- Arming")
         await self.antl_drone.action.arm()
         await asyncio.sleep(3)
