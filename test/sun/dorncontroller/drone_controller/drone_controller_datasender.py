bddb4a2901b97a7e2ecda31da69810d41cb00c59
sunjuwhan <mathsun0011@gmail.com>
2024-04-10 14:19:03 +0900

    test

--- test/sun/dorncontroller/drone_controller/drone_controller_datasender.py
+++ test/sun/dorncontroller/drone_controller/drone_controller_datasender.py
@@ -14,7 +14,7 @@ class class_drone_controller_datasender:
     def send_joystick_data(self, data):
         try:
             # 데이터를 직렬화하고 전송
-            
+            print(data)
             self.socket.sendall(data.encode())
         except Exception as e:
             print(f"Error sending joystick data: {e}")
