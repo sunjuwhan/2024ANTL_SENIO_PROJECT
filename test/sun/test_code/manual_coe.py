a1692c00170a515f79f641d52158b2bad8d287fc
sunjuwhan <mathsun0011@gmail.com>
2024-04-10 21:45:04 +0900

    Update manual_coe.py

--- test/sun/test_code/manual_coe.py
+++ test/sun/test_code/manual_coe.py
@@ -75,7 +75,8 @@ async def manual_controls():
     # start manual control
     #print("-- Starting manual control")
     #await drone.manual_control.start_position_control()
-
+    print("-- Starting manual control")
+    await drone.manual_control.start_position_control()
     while True:
         try:
             data=sock.recv(300).decode()
