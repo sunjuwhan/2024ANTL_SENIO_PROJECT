38d149c73e2c7f8836df0103ac61941c92daf58f
sunjuwhan <mathsun0011@gmail.com>
2024-04-12 20:16:56 +0900

    tset

--- test/sun/test_code/mission.py
+++ test/sun/test_code/mission.py
@@ -72,6 +72,27 @@ async def run():
     print("-- Starting mission")
     await drone.mission.start_mission()
     print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx\nxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    mission_items.pop()
+    mission_items.append(MissionItem(47.398039859999997,
+                                    8.5455725400000002,
+                                    25,
+                                    10,
+                                    True,
+                                    float('nan'),
+                                    float('nan'),
+                                    MissionItem.CameraAction.NONE,
+                                    float('nan'),
+                                    float('nan'),
+                                    float('nan'),
+                                    float('nan'),
+                                    float('nan'),
+                                    MissionItem.VehicleAction.NONE))
+
+    mission_plan=MissionPlan(mission_items)
+    await drone.mission.set_return_to_launch_after_mission(False)
+    await drone.mission.upload_mission(mission_plan)
+    print("two ")
+    await drone.mission.start_mission()
     await termination_task
     
 async def get_gps(drone,gpsmodel:GpsModel) :
