22621c62eac29667ee0149e6334fd5d1016244eb
sunjuwhan <mathsun0011@gmail.com>
2024-04-12 19:58:25 +0900

    Update mission.py

--- test/sun/test_code/mission.py
+++ test/sun/test_code/mission.py
@@ -4,29 +4,25 @@ import asyncio
 
 from mavsdk import System
 from mavsdk.mission import (MissionItem, MissionPlan)
-
-
-async def run():
-    drone = System()
-    await drone.connect(system_address="udp://:14540")
-
-    print("Waiting for drone to connect...")
-    async for state in drone.core.connection_state():
-        if state.is_connected:
-            print(f"-- Connected to drone!")
-            break
-
-    print_mission_progress_task = asyncio.ensure_future(
-        print_mission_progress(drone))
-
-    running_tasks = [print_mission_progress_task]
-    termination_task = asyncio.ensure_future(
-        observe_is_in_air(drone, running_tasks))
-
+import time
+class GpsModel():
+    def __init__(self) -> None:
+        self.latitude_deg=0.0
+        self.longitude_deg=0.0
+        self.absolute_altitude_m=0.0
+        self.relative_altitude_m=0.0
+    def set_gps(self,lat,log,abso,rel):
+        self.latitude_deg=lat
+        self.longitude_deg=log
+        self.absolute_altitude_m=abso
+        self.relative_altitude_m=rel
+    def get_gps(self):
+        return (self.latitude_deg,self.longitude_deg,self.absolute_altitude_m,self.relative_altitude_m)
+async def upload_and_execute_mission(drone,longitude,latitude,relative):
     mission_items = []
-    mission_items.append(MissionItem(47.398039859999997,
-                                     8.5455725400000002,
-                                     25,
+    mission_items.append(MissionItem(longitude,
+                                     latitude,
+                                     relative,
                                      10,
                                      True,
                                      float('nan'),
@@ -41,10 +37,32 @@ async def run():
     mission_plan = MissionPlan(mission_items)
 
     await drone.mission.set_return_to_launch_after_mission(False)
-
     print("-- Uploading mission")
     await drone.mission.upload_mission(mission_plan)
+    print("-- Starting mission")
+    await drone.mission.start_mission()
 
+async def run():
+    drone = System()
+    gps_model=GpsModel()
+    await drone.connect(system_address="udp://:14540")
+
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"-- Connected to drone!")
+            break
+
+    
+    print_mission_progress_task = asyncio.ensure_future(
+        print_mission_progress(drone))
+
+    running_tasks = [print_mission_progress_task]
+    termination_task = asyncio.ensure_future(
+        observe_is_in_air(drone, running_tasks))
+
+  
+    
     print("Waiting for drone to have a global position estimate...")
     async for health in drone.telemetry.health():
         if health.is_global_position_ok and health.is_home_position_ok:
@@ -53,13 +71,19 @@ async def run():
 
     print("-- Arming")
     await drone.action.arm()
+    asyncio.ensure_future(get_gps(drone,gps_model))
+    data=gps_model.get_gps() 
+    while True:
+        await upload_and_execute_mission(drone,data[0],data[1],data[3])
+        time.sleep(1) 
+        
 
-    print("-- Starting mission")
-    await drone.mission.start_mission()
-
-    await termination_task
-
-
+        
+async def get_gps(drone,gpsmodel:GpsModel) :
+    async for position in drone.telemetry.position():
+        gpsmodel.set_gps(position.latitude_deg,position.longitude_deg,position.absolute_altitude_m,
+                                     position.relative_altitude_m)
+    
 async def print_mission_progress(drone):
     async for mission_progress in drone.mission.mission_progress():
         print(f"Mission progress: "
