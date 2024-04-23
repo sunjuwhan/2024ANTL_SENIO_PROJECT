9e53687054def0d483528aac28d23c586e8177b3
sunjuwhan <mathsun0011@gmail.com>
2024-04-23 13:14:34 +0900

    Update goto_gps.py

--- test/sun/test_code/goto_gps.py
+++ test/sun/test_code/goto_gps.py
@@ -109,7 +109,7 @@ async def run():
     await drone.action.takeoff()
     await asyncio.sleep(5)
     try:
-        await self.antl_drone.manual_control.set_manual_control_input(
+        await drone.manual_control.set_manual_control_input(
         float(0), float(0), float(0.5), float(0)
     )
         print("good")
