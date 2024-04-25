1e55a332d14077b037eaf1c6fa76e8e736d32fca
sunjuwhan <mathsun0011@gmail.com>
2024-04-25 19:45:18 +0900

    Update drone_controller_switch.py

--- test/sun/dorncontroller/drone_controller/drone_controller_switch.py
+++ test/sun/dorncontroller/drone_controller/drone_controller_switch.py
@@ -5,8 +5,8 @@ from drone_controller.drone_controller_information import *
 class class_Drone_Controller_Switch:
     def __init__(self, info):
         self.info = info
-        self.switch1_pin = 11  # 보드 기준 핀 번호
-        self.switch2_pin = 13
+        self.switch1_pin = 13  # 보드 기준 핀 번호
+        self.switch2_pin = 11
         self.switch3_pin = 36
         self.switch4_pin = 37
         GPIO.setmode(GPIO.BOARD)
@@ -20,9 +20,9 @@ class class_Drone_Controller_Switch:
             # 현재 스위치 상태 읽기
             self.info.switch1 = True if GPIO.input(self.switch1_pin) == GPIO.HIGH else False    #왼쪽위 
             
-            self.info.switch2 = True if GPIO.input(self.switch1_pin) == GPIO.HIGH else False    #왼쪽 아래
+            self.info.switch2 = True if GPIO.input(self.switch2_pin) == GPIO.HIGH else False    #왼쪽 아래
             
-            self.info.switch3 = True if GPIO.input(self.switch1_pin) == GPIO.HIGH else False   #오른쪽 위
+            self.info.switch3 = True if GPIO.input(self.switch3_pin) == GPIO.HIGH else False   #오른쪽 위
             
-            self.info.switch4 = True if GPIO.input(self.switch1_pin) == GPIO.HIGH else False  #오른쪽 아래 
+            self.info.switch4 = True if GPIO.input(self.switch4_pin) == GPIO.HIGH else False  #오른쪽 아래 
             time.sleep(0.05)
