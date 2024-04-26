8960c12fc3549d4993c7f98d720545ae644817b0
sunjuwhan <mathsun0011@gmail.com>
2024-04-26 15:33:51 +0900

    test

--- test/sun/test_code/camera/client_camera.py
+++ test/sun/test_code/camera/client_camera.py
@@ -2,34 +2,34 @@ import socket
 import cv2
 import time
 import numpy as np
-
+from picamera2 import Picamera2
 UDP_IP = "192.168.50.47"
 UDP_PORT = 9505
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-cap = cv2.VideoCapture(0)
 
 # 카메라 해상도 설정
-cap.set(3, 320)  # 가로 해상도
-cap.set(4, 240)  # 세로 해상도
-
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (320, 240)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start()
 while True:
-    ret, frame = cap.read()
-    if ret:
-        # 프레임을 JPEG 형식으로 압축
-        _, encoded_frame = cv2.imencode('.jpg', frame)
-        
-        # 압축된 이미지 데이터를 바이트 스트링으로 변환
-        s = encoded_frame.tobytes()
+    frame = picam2.capture_array()
+    # 프레임을 JPEG 형식으로 압축
+    _, encoded_frame = cv2.imencode('.jpg', frame)
+    
+    # 압축된 이미지 데이터를 바이트 스트링으로 변환
+    s = encoded_frame.tobytes()
 
-        # 바이트 스트링을 여러 패킷으로 나누어 전송
-        for i in range(5):
-            sock.sendto(bytes([i]) + s[i * 46080:(i + 1) * 46080], (UDP_IP, UDP_PORT))  # JPEG 압축된 프레임 전송
+    # 바이트 스트링을 여러 패킷으로 나누어 전송
+    for i in range(5):
+        sock.sendto(bytes([i]) + s[i * 46080:(i + 1) * 46080], (UDP_IP, UDP_PORT))  # JPEG 압축된 프레임 전송
 
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-cap.release()
 cv2.destroyAllWindows()
