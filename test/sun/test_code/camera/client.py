beb86a00aa8263680e6fb2dd505b551051b45392
sunjuwhan <mathsun0011@gmail.com>
2024-04-26 14:36:20 +0900

    Update client.py

--- test/sun/test_code/camera/client.py
+++ test/sun/test_code/camera/client.py
@@ -1,7 +1,7 @@
 
 import socket
 import cv2
-
+import time
 UDP_IP = '192.168.50.47'
 UDP_PORT = 9505
 
@@ -13,10 +13,11 @@ while True:
     ret, frame = cap.read()
     d = frame.flatten()
     s = d.tostring()
-
+    start_time=time.time()
     for i in range(20):
         sock.sendto(bytes([i]) + s[i*46080:(i+1)*46080], (UDP_IP, UDP_PORT))
-
+    end_time=time.time()
+    print(end_time-start_time)
  
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
