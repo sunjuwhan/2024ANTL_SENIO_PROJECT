c443bdc0b76575698d3c0ae432a587e8585dbd57
sunjuwhan <mathsun0011@gmail.com>
2024-04-29 20:40:59 +0900

    Update drone_controller_display.py

--- test/sun/dorncontroller/drone_controller/drone_controller_display.py
+++ test/sun/dorncontroller/drone_controller/drone_controller_display.py
@@ -74,12 +74,12 @@ class class_drone_controller_display:
         self.update_switches()
         self.update_gps()
         self.update_joystick()  # Add call to update_joystick method
-        #self.update()
+        self.update()
         self.window.mainloop()
 
     def update(self):
         #frame = cv2.resize(self.info_frame, (640, 480))  # Resize frame to 640x480
-        frame = self.info_frame  #--> TK FRAME 으로 변환완료 되었고 
+        frame = self.info.frame  #--> TK FRAME 으로 변환완료 되었고 
         # NumPy 배열을 PIL 이미지로 변환
         pil_image = Image.fromarray(frame)
 
