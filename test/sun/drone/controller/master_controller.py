72c6e62ec27dc0b7990340a8fdf1254ac1fdbe7f
sunjuwhan <mathsun0011@gmail.com>
2024-04-29 21:27:55 +0900

    Update master_controller.py

--- test/sun/drone/controller/master_controller.py
+++ test/sun/drone/controller/master_controller.py
@@ -16,8 +16,9 @@ class MasterController():
         self.__camera_controller.run()
     async def run_pilot(self):    #asyncio .run()으로 실행하고 나머지는 thread로 실행해야할거같은데
         try:
-            self.__pilot_controller=PilotController(self.__pilot_model,self.__gps_model)
-            print("start init drone")
+            print("hello")
+            #self.__pilot_controller=PilotController(self.__pilot_model,self.__gps_model)
+            #print("start init drone")
             #await self.__pilot_controller.init_dron()
             #self.__drone=self.__pilot_controller.get_dron_from_controller()  
             #await self.__pilot_controller.run()  #controller 시작하고 
