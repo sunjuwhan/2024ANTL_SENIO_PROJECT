a0c1bbc2deaf3bb2557e42b6f68f2dc2e01b6890
sunjuwhan <mathsun0011@gmail.com>
2024-04-29 21:34:39 +0900

    test

--- test/sun/drone/controller/master_controller.py
+++ test/sun/drone/controller/master_controller.py
@@ -14,6 +14,7 @@ class MasterController():
         self.__gps_controller=None
     def run_camera(self):
         self.__camera_controller.run()
+        
     async def run_pilot(self):    #asyncio .run()으로 실행하고 나머지는 thread로 실행해야할거같은데
         try:
             print("hello")
--- test/sun/drone/model/pilot_model.py
+++ test/sun/drone/model/pilot_model.py
@@ -22,12 +22,14 @@ class Drone:
                 print("-- Global position state is good enough for flying.")
                 break
         try:
-            await self.antl_drone.manual_control.set_manual_control_input(
-            float(0), float(0), float(0.5), float(0)
-        )
+            await self.antl_drone.manual_control.set_manual_control_input
+            (float(0), float(0), float(0.5), float(0))
+            
             print("good")
+            
         except Exception as e:
             print(e)
+            
         #print("-- Arming")
         #await self.antl_drone.action.arm()
         #await asyncio.sleep(1)
