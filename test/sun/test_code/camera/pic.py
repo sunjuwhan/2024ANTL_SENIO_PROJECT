40ea433edb930d9eb19b7c3aa4735a2737b67315
sunjuwhan <mathsun0011@gmail.com>
2024-04-29 20:58:56 +0900

    Update pic.py

--- test/sun/test_code/camera/pic.py
+++ test/sun/test_code/camera/pic.py
@@ -20,13 +20,7 @@ picam2.preview_configuration.main.format = "RGB888"
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
-
-im= picam2.capture_array()
-print(im)
-print(type(im))
-cv2.imshow("Camera", im)
-cv2.imwrite("output.jpg",im)
-with open("output.jpg",'rb') as im:
-    image_bytes=im.read()
+while True:
+    im= picam2.capture_array()
+    cv2.imshow("Camera", im)
     
-print(image_bytes)
