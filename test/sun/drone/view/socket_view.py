a9739b1e2e9845cffda9058b040105cf451587c9
sunjuwhan <mathsun0011@gmail.com>
2024-04-29 20:49:33 +0900

    Update socket_view.py

--- test/sun/drone/view/socket_view.py
+++ test/sun/drone/view/socket_view.py
@@ -33,7 +33,7 @@ class SocketView():
         try:
             while True : 
                 frame=self.__video_model.get_send_frame()  #46080
-                print(type(frame))
+                cv2.imshow("frame",frame)
                 _, encoded_frame=cv2.imencode('.jpg',frame)
                 s=encoded_frame.tobytes()
                 
