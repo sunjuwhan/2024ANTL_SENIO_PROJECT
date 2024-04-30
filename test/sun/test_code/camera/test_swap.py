4816052f130b761b25c06b3abcdef84fcd3c1fd5
sunjuwhan <mathsun0011@gmail.com>
2024-04-30 20:45:27 +0900

    Update test_swap.py

--- test/sun/test_code/camera/test_swap.py
+++ test/sun/test_code/camera/test_swap.py
@@ -15,8 +15,8 @@ device = pipeline_profile.get_device()
 device_product_line = str(device.get_info(rs.camera_info.product_line))
 
         # RGB & Depth
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 320, 240, rs.format.z16, 30)
+config.enable_stream(rs.stream.color, 320, 240, rs.format.bgr8, 30)
 raw_frame=None
 
 picam2 = Picamera2()
