f4fd0b069e1e5968d8e676253c8a4b6a875debee
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-05-01 16:49:36 +0900

    Update

--- test/sun/dorncontroller/drone_controller/drone_controller_videostreamer.py
+++ test/sun/dorncontroller/drone_controller/drone_controller_videostreamer.py
@@ -34,7 +34,7 @@ class class_Drone_Controller_VideoStreamer:
 
                 # 바이트 스트링을 numpy 배열로 변환하고 이미지로 디코딩
                 frame = cv2.imdecode(np.frombuffer(picture, dtype=np.uint8), cv2.IMREAD_COLOR)
-                cv2.imshow("frame", frame)
+                #cv2.imshow("frame", frame)
                 self.info.frame = cv2.cvtColor(cv2.COLOR_BGR2RGB)
                 # 프레임 표시 시간 계산
 
