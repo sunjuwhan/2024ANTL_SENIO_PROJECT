b44cff07a844afe6c164239563b7eb7ce3ea70e6
ANTL-KJH <119037634+ANTL-KJH@users.noreply.github.com>
2024-05-02 11:30:38 +0900

    Update drone_controller_display.py

--- test/sun/dorncontroller/drone_controller/drone_controller_display.py
+++ test/sun/dorncontroller/drone_controller/drone_controller_display.py
@@ -6,8 +6,6 @@ import threading
 import random
 import string
 from drone_controller.drone_controller_information import *
-import time
-
 class class_drone_controller_display_master:
     def __init__(self, info):
         self.dc_display = None
@@ -15,7 +13,6 @@ class class_drone_controller_display_master:
 
     def run_display(self):
         self.dc_display = class_drone_controller_display(self.info)
-        self.dc_display.run_display()
 
 class class_drone_controller_display:
     def __init__(self, info):
@@ -72,79 +69,60 @@ class class_drone_controller_display:
         self.log_text_R = tk.Text(self.info_frame, width=22, height=7, bg="#1c1c1c", fg="white", font=("Arial", 8))
         self.log_text_R.pack(anchor="w", padx=8, pady=(4, 0))
 
-        self.setup_threads()
+        self.update_gps()
+        self.update_switches()
+        self.update_joystick()
+        self.update_video()
         self.window.mainloop()
 
-    def setup_threads(self):
-        # Create threads for updating GPS, Switches, and Joystick
-        self.gps_thread = threading.Thread(target=self.update_gps)
-        self.switch_thread = threading.Thread(target=self.update_switches)
-        self.joystick_thread = threading.Thread(target=self.update_joystick)
-        self.video_thread = threading.Thread(target=self.update_video)
-
-        # Start threads
-        self.gps_thread.start()
-        self.switch_thread.start()
-        self.joystick_thread.start()
-        self.video_thread.start()
 
     def update_video(self):
-        while True:
-            frame = self.info.frame
-            pil_image = Image.fromarray(frame)
-            resized_image = pil_image.resize((640, 480))  # 원하는 크기로 이미지 리사이즈
+        frame = self.info.frame
+        pil_image = Image.fromarray(frame)
+        resized_image = pil_image.resize((640, 480))  # 원하는 크기로 이미지 리사이즈
 
-            # PIL 이미지를 PhotoImage로 변환
-            self.photo = ImageTk.PhotoImage(image=resized_image)
-            self.frame_canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-            time.sleep(0.05)
-
-    def update_gps(self):
-        while True:
-            latitude_text = f"Latitude: {self.info.drone_latitude:.5f}"
-            longitude_text = f"Longitude: {self.info.drone_longitude:.5f}"
-
-            self.latitude_label.config(text=latitude_text)
-            self.longitude_label.config(text=longitude_text)
-
-            # Sleep for some time before updating again
-            time.sleep(1)
+        # PIL 이미지를 PhotoImage로 변환
+        self.photo = ImageTk.PhotoImage(image=resized_image)
+        self.frame_canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+        self.window.after(50, self.update_video)
 
     def update_switches(self):
-        while True:
-            for label in self.switch_labels:
-                label.destroy()
+        for label in self.switch_labels:
+            label.destroy()
 
-            self.switch_labels = []
-            for i in range(1, 5):  # 1부터 4까지 반복
-                switch_value = getattr(self.info, f'switch{i}')  # self.info.switch1, self.info.switch2 등을 가져옴
-                switch_label = tk.Label(self.switch_frame, text=f"Switch {i}: {switch_value}",
-                                        anchor="w", bg="#404040", fg="white", font=("Arial", 8))  # White text color
-                switch_label.pack(anchor="w", padx=8)
-                self.switch_labels.append(switch_label)
+        self.switch_labels = []
+        for i in range(1, 5):  # 1부터 4까지 반복
+            switch_value = getattr(self.info, f'switch{i}')  # self.info.switch1, self.info.switch2 등을 가져옴
+            switch_label = tk.Label(self.switch_frame, text=f"Switch {i}: {switch_value}",
+                                    anchor="w", bg="#404040", fg="white", font=("Arial", 8))  # White text color
+            switch_label.pack(anchor="w", padx=8)
+            self.switch_labels.append(switch_label)
+        self.window.after(500, self.update_switches)
 
-            # Sleep for some time before updating again
-            time.sleep(0.3)
+    def update_gps(self):
+        latitude_text = f"Latitude: {self.info.drone_latitude:.5f}"
+        longitude_text = f"Longitude: {self.info.drone_longitude:.5f}"
+
+        self.latitude_label.config(text=latitude_text)
+        self.longitude_label.config(text=longitude_text)
+        self.window.after(2000, self.update_gps)
 
     def update_joystick(self):
-        while True:
-            # Simulate joystick data
-            joystick_values_L = {
-                'x': self.info.joystick_Left_x,
-                'y': self.info.joystick_Left_y,
-                'switch': self.info.joystick_Left_val
-            }
-            joystick_values_R = {
-                'x': self.info.joystick_Right_x,
-                'y': self.info.joystick_Right_y,
-                'switch': self.info.joystick_Right_val
-            }
+        # Simulate joystick data
+        joystick_values_L = {
+            'x': self.info.joystick_Left_x,
+            'y': self.info.joystick_Left_y,
+            'switch': self.info.joystick_Left_val
+        }
+        joystick_values_R = {
+            'x': self.info.joystick_Right_x,
+            'y': self.info.joystick_Right_y,
+            'switch': self.info.joystick_Right_val
+        }
 
-            self.update_joystick_labels(self.joystick_frame_L, "Joystick L", joystick_values_L)
-            self.update_joystick_labels(self.joystick_frame_R, "Joystick R", joystick_values_R)
-
-            # Sleep for some time before updating again
-            time.sleep(0.1)
+        self.update_joystick_labels(self.joystick_frame_L, "Joystick L", joystick_values_L)
+        self.update_joystick_labels(self.joystick_frame_R, "Joystick R", joystick_values_R)
+        self.window.after(300, self.update_joystick)
 
     def update_joystick_labels(self, frame, name, values):
         for label in frame.winfo_children():
@@ -163,6 +141,8 @@ class class_drone_controller_display:
                                 bg="#404040", fg="white", font=("Arial", 8))
         switch_label.pack(anchor="w", padx=(8, 0))
 
+
+
+
 if __name__ == "__main__":
-    dc_display = class_drone_controller_display_master(class_Drone_Controller_Information())
-    dc_display.run_display()
+    dc_display = class_drone_controller_display(class_Drone_Controller_Information())
